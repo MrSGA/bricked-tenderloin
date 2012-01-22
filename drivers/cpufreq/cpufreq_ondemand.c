faux123/pyramid-2.6.35_sense

cpufreq: Fix input handler crash
When input handler is running on a CPU not in ondemand
governor, skip making CPU frequency changes.

CRs-Fixed: 299451
Signed-off-by: David Ng <dave@codeaurora.org>
(cherry picked from commit 88a65c7ae04632ffee11f9fc628d7ab017c06b83)

Change-Id: Ibf04b210aea62f5c53c0ba0482615f94cf29fc55
Signed-off-by: Sunil Joseph <sunilj@codeaurora.org>
modified by faux123

drivers/cpufreq/cpufreq_ondemand.c
@@ -749,20 +749,28 @@ static void dbs_refresh_callback(struct work_struct *unused)
 {
 	struct cpufreq_policy *policy;
 	struct cpu_dbs_info_s *this_dbs_info;
+	unsigned int cpu = smp_processor_id();
 
-	if (trylock_policy_rwsem_write(0) < 0)
+	if (trylock_policy_rwsem_write(cpu) < 0)
 		return;
 
-	this_dbs_info = &per_cpu(od_cpu_dbs_info, 0);
+	this_dbs_info = &per_cpu(od_cpu_dbs_info, cpu);
 	policy = this_dbs_info->cur_policy;
+	if (!policy) {
+		/* CPU not using ondemand governor */
+		unlock_policy_rwsem_write(cpu);
+		return;
+	}
 
 	if (policy->cur < policy->max) {
+		policy->cur = policy->max;
+
 		__cpufreq_driver_target(policy, policy->max,
 					CPUFREQ_RELATION_L);
-		this_dbs_info->prev_cpu_idle = get_cpu_idle_time(0,
+		this_dbs_info->prev_cpu_idle = get_cpu_idle_time(cpu,
 				&this_dbs_info->prev_cpu_wall);
 	}
-	unlock_policy_rwsem_write(0);
+	unlock_policy_rwsem_write(cpu);
 }
 
 static DECLARE_WORK(dbs_refresh_work, dbs_refresh_callback);

MrSGA/bricked-tenderloin

Conflicts:
	drivers/cpufreq/cpufreq_ondemand.c

drivers/cpufreq/cpufreq_ondemand.c
@@ -651,22 +651,28 @@ static void dbs_refresh_callback(struct work_struct *unused)
 {
 	struct cpufreq_policy *policy;
 	struct cpu_dbs_info_s *this_dbs_info;
+	unsigned int cpu = smp_processor_id();
 
-	if (lock_policy_rwsem_write(0) < 0)
+	if (lock_policy_rwsem_write(cpu) < 0)
 		return;
 
-	this_dbs_info = &per_cpu(od_cpu_dbs_info, 0);
+	this_dbs_info = &per_cpu(od_cpu_dbs_info, cpu);
 	policy = this_dbs_info->cur_policy;
+	if (!policy) {
+		/* CPU not using ondemand governor */
+		unlock_policy_rwsem_write(cpu);
+		return;
+	}
 
 	if (policy->cur < policy->max) {
 		policy->cur = policy->max;
 
 		__cpufreq_driver_target(policy, policy->max,
 					CPUFREQ_RELATION_L);
-		this_dbs_info->prev_cpu_idle = get_cpu_idle_time(0,
+		this_dbs_info->prev_cpu_idle = get_cpu_idle_time(cpu,
 				&this_dbs_info->prev_cpu_wall);
 	}
-	unlock_policy_rwsem_write(0);
+	unlock_policy_rwsem_write(cpu);
 }
 
 static DECLARE_WORK(dbs_refresh_work, dbs_refresh_callback);